22772e27e8b0bbd5eb0aed2eb94e40b8dedcfbdb
Joskeiner simosa Urdaneta <Joskeiner.simosa9@gmail.com>
2023-03-18 01:31:45 -0300

    creacion de fuciones para el manejo de datos en postgres y  conexion a la DB

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -7,28 +7,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/joskeiner/go_and_Postgres/models"
 )
 
+// objeto para enviar el response
 type response struct {
 	ID      int64  `jsion:"id,omitempty"`
 	Message string `json:"message,omitempty"`
 }
 
+// crear connection with postgres db
 func CreateConnection() *sql.DB {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file ")
 	}
-
+	// abrir la coneccion
 	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
 
 	if err != nil {
 		panic(err)
 	}
-
+	//	check db
 	err = db.Ping()
 
 	if err != nil {
@@ -37,23 +41,142 @@ func CreateConnection() *sql.DB {
 
 	fmt.Println("Successfully connected to postgres")
 	return db
-
+	// devolver la conecion
 }
-func GetStock(w http.ResponseWriter, r *http.Request) {
+
+// la funcion crea un stock en la  base de datos de postgres
+func CreateStock(w http.ResponseWriter, r *http.Request) {
+	//crear un stock vacio de tpi models.Stock
 	var stock models.Stock
 
-	json.NewDecoder(r.Body).Decode(&stock)
+	//decodificar la peticion para stock
+	err := json.NewDecoder(r.Body).Decode(&stock)
+	if err != nil {
+		log.Fatalf("Unable to decode the request body . %v", err)
+	}
+	// llama a la funcion  insert Stock  y pasa la struct
+	insertID := insertStock(stock)
+
+	//formato de respuesta
+	res := response{
+		ID:      insertID,
+		Message: "Stock created successfully",
+	}
+
+	// envio de respuesta
+	json.NewEncoder(w).Encode(res)
 
 }
-func GetAllStock() {
+
+// GetAllStock trera todos los stock
+func GetAllStock(w http.ResponseWriter, r *http.Request) {
+	//traer todos los stock de la base de datos
+	stock, err := getAllStock()
+
+	//en caso  de un error
+	if err != nil {
+		log.Fatalf("Unable to get all stock %v", err)
+	}
+
+	//envio de todos los stocks en formato jsoon
+	json.NewEncoder(w).Encode(stock)
 
 }
-func CreateStock() {
+
+// GetStock retornara un stock por id
+func GetStock(w http.ResponseWriter, r *http.Request) {
+	// obtener la variable stockid desde el request
+	paramas := mux.Vars(r)
+
+	//convertir the id de string a int
+	//para trabajar con el
+	id, err := strconv.Atoi(paramas["id"])
+
+	if err != nil {
+		log.Fatalf("unable to convert the string int int %v", err)
+	}
+	//llama a la funcion getStock con el id del strcok a buscar
+	stock, err := getStock(int64(id))
+
+	if err != nil {
+		log.Fatalf("uncable to get stock .%v", err)
+	}
+
+	// enviar el json
+	json.NewEncoder(w).Encode(stock)
 
 }
-func UpdateStock() {
+
+// actualizara un  stock en postgres db
+func UpdateStock(w http.ResponseWriter, r *http.Request) {
+
+	// obtener stockid desde r , la llave es 'id'
+	paramas := mux.Vars(r)
+
+	//covertir el id de tipo string a int
+	id, err := strconv.Atoi(paramas["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to decode the request body. %v", err)
+	}
+	//crear un struc de stock
+	var stock models.Stock
+	err = json.NewDecoder(r.Body).Decode(&stock)
+	if err != nil {
+		log.Fatalf("Unable to decode the request body. %v ", err)
+	}
+
+	// llamar a la funcion stock para actualizar the stock
+	updatedRows := updateStock(int64(id), stock)
+
+	msg := fmt.Sprintf("Stock update successfully . Total rows/record affected %v", updatedRows)
+
+	// strcut de respose
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	// envio de json
+	json.NewEncoder(w).Encode(res)
+}
+
+// elimina una fila de la base de datos
+func DaleteStock(w http.ResponseWriter, r *http.Request) {
+
+	//trae the stockid desde el request
+	params := mux.Vars(r)
+
+	// convierte el id de string a int
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to covert the string into int . %v", err)
+	}
+
+	// llama  al la funcion deleteStock , convierte el int en int64
+	deletedRows := deleteStock(int64(id))
+
+	// mensaje
+	msg := fmt.Sprintf("Stock updated successfully. Total rows/record affected %v", deletedRows)
+
+	//struct para  enviar
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	//envio de json
+
+	json.NewEncoder(w).Encode(res)
 
 }
-func DaleteStock() {
+
+//----------------------------handler funcions ------------------------------------------------
+
+// insetar un stock en la DB
+func insertStock(stock models.Stock) int64 {
+	db := CreateConnection()
+	defer db.Close()
 
 }
